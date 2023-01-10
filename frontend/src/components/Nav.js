c0d840c26bce5b9f2905a26b6be8a833ad30582a
eprescappy <wurtht@gain.uni-sopron.hu>
2023-01-10 12:04:46 +0100

    Navbar with logo and auth navitem changes added.

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,38 +1,56 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Nav = () => {
+  const auth = localStorage.getItem('token')
+  const navigate = useNavigate()
+  const logout = () => {
+    localStorage.clear()
+    navigate('/login')
+  }
   return (
     <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/songs">Songs</Link>
-        </li>
-        <li>
-          <Link to="/upload-song">Upload song</Link>
-        </li>
-        <li>
-          <Link to="/categories">Categories</Link>
-        </li>
-        <li>
-          <Link to="/create-categories">Create categories</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-      </ul>
+      <img
+        className="navbarLogo"
+        alt="logo"
+        src="https://www.nicepng.com/png/full/9-96506_music-transparent-wave-electronic-music-png.png"
+      />
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/songs">Songs</Link>
+          </li>
+          <li>
+            <Link to="/upload-song">Upload song</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+          <li>
+            <Link to="/create-categories">Create categories</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/login">
+              Logout
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
